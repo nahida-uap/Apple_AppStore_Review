ee7b7c990dd63c8cdb0bf4b09b9a3864ea2002ff
Nahida Sultana Chowdhury <nchowdhury@uap-bd.edu>
2019-07-26 14:59:40 -0400

    Update appStoreReviewExtractor.py

--- appStoreReviewExtractor.py
+++ appStoreReviewExtractor.py
@@ -16,38 +16,30 @@ def getJson(url):
 
 def getReviews(appID, page=1):
     try:
-        url = 'https://itunes.apple.com/rss/customerreviews/id=%s/page=%d/sortby=mostrecent/json' % (appID, page)
-        #print("test1")
+        url = 'https://itunes.apple.com/rss/customerreviews/id=%s/page=%d/sortby=mostrecent/json' % (appID, page)        
         data = getJson(url).get('feed')
-        #print("test2")
         if data.get('entry') == None:
             getReviews(appID, page+1)
             return
-        #print("test3")
+        
         for entry in data.get('entry'):
-            #print("test")
             if entry.get('im:name'):
                 continue
             #review_id = entry.get('id').get('label')
             title = entry.get('title').get('label')
             #print(title)
             
-            author = entry.get('author').get('name').get('label')
-            #author_url = entry.get('author').get('uri').get('label')
+            author = entry.get('author').get('name').get('label')            
             version = entry.get('im:version').get('label')
             rating = entry.get('im:rating').get('label')
             review = entry.get('content').get('label')	
             vote_count = entry.get('im:voteCount').get('label')
             
             csvData = [title.encode("utf-8"),  author.encode("utf-8"), version.encode("utf-8"), rating.encode("utf-8"), review.encode("utf-8"), vote_count]
-            #writer.writerow(unicode(csvData).encode("utf-8") )
             writer.writerow(csvData)
-            
-            #print('"'+'","'.join(csvData)+'"')
             print(csvData)
         
-        if page<=10:
-            print ("\n\n\n"+str(page)+"\n\n\n")
+        if page<=10:            
             getReviews(appID, page+1)
     
     except Exception:
@@ -58,7 +50,6 @@ csvTitles = ['title',  'author', 'version', 'rating', 'review', 'vote_count']
 myFile = open('reviews.csv',"w")
 with myFile:
     writer = csv.writer(myFile)
-    writer.writerow(csvTitles)
-    #print ','.join(csvTitles)
+    writer.writerow(csvTitles)    
     getReviews(920869470)
     myFile.close()
